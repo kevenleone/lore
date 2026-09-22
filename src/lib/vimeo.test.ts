6699e5a58b85b513d33745c12416ff07654a5d1a
Keven Leone <kevenleone.me@gmail.com>
2026-09-21 21:51:00 -0300

    test: use a public Vimeo video in examples

--- src/lib/vimeo.test.ts
+++ src/lib/vimeo.test.ts
@@ -4,20 +4,20 @@ import { vimeoEmbedUrl, vimeoVideo } from './vimeo';
 
 describe('vimeoVideo', () => {
     it.each([
-        ['https://vimeo.com/1211345949', { id: '1211345949' }],
-        ['vimeo.com/1211345949', { id: '1211345949' }],
+        ['https://vimeo.com/180094337', { id: '180094337' }],
+        ['vimeo.com/180094337', { id: '180094337' }],
         [
-            'https://player.vimeo.com/video/1211345949?title=0&byline=0&portrait=0&autoplay=1&autopause=0&dnt=1&app_id=122963&texttrack=undefined',
-            { id: '1211345949' },
+            'https://player.vimeo.com/video/180094337?title=0&byline=0&portrait=0&autoplay=1&autopause=0&dnt=1&app_id=122963&texttrack=undefined',
+            { id: '180094337' },
         ],
-        ['https://vimeo.com/1211345949/a1b2c3d4e5', { hash: 'a1b2c3d4e5', id: '1211345949' }],
+        ['https://vimeo.com/180094337/a1b2c3d4e5', { hash: 'a1b2c3d4e5', id: '180094337' }],
         [
-            'https://player.vimeo.com/video/1211345949?h=a1b2c3d4e5',
-            { hash: 'a1b2c3d4e5', id: '1211345949' },
+            'https://player.vimeo.com/video/180094337?h=a1b2c3d4e5',
+            { hash: 'a1b2c3d4e5', id: '180094337' },
         ],
-        ['https://vimeo.com/1211345949#t=90s', { id: '1211345949', start: 90 }],
-        ['https://vimeo.com/1211345949#t=1m30s', { id: '1211345949', start: 90 }],
-        ['https://vimeo.com/showcase/9876543/video/1211345949', { id: '1211345949' }],
+        ['https://vimeo.com/180094337#t=90s', { id: '180094337', start: 90 }],
+        ['https://vimeo.com/180094337#t=1m30s', { id: '180094337', start: 90 }],
+        ['https://vimeo.com/showcase/9876543/video/180094337', { id: '180094337' }],
     ])('reads %s', (url, video) => {
         expect(vimeoVideo(url)).toEqual(video);
     });
@@ -34,8 +34,8 @@ describe('vimeoVideo', () => {
 
 describe('vimeoEmbedUrl', () => {
     it('carries the privacy hash and start time through', () => {
-        expect(vimeoEmbedUrl({ hash: 'a1b2c3d4e5', id: '1211345949', start: 90 })).toBe(
-            'https://player.vimeo.com/video/1211345949?autoplay=1&dnt=1&h=a1b2c3d4e5#t=90s',
+        expect(vimeoEmbedUrl({ hash: 'a1b2c3d4e5', id: '180094337', start: 90 })).toBe(
+            'https://player.vimeo.com/video/180094337?autoplay=1&dnt=1&h=a1b2c3d4e5#t=90s',
         );
     });
 });
